7d49e0e053133cec31f6740d4add3d15dc7b782c
Jason <jmartgmz@upm.me>
2025-04-16 00:34:21 -0400

    New configuration file for bookmarks

---
+++ config.js
@@ -0,0 +1,56 @@
+// Navigation Configuration File
+const navConfig = {
+  // Blue category
+  frequent: {
+    title: "title",
+    color: "blue",
+    links: [
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" }
+    ]
+  },
+  // Green category
+  fourchan: {
+    title: "title",
+    color: "green",
+    links: [
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" }
+    ]
+  },
+  // Red category
+  entertainment: {
+    title: "title",
+    color: "red",
+    links: [
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" }
+    ]
+  },
+  // Yellow category
+  miscellaneous: {
+    title: "title",
+    color: "yellow",
+    links: [
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" }
+    ]
+  },
+  // Red category
+  other: {
+    title: "title",
+    color: "red",
+    links: [
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" },
+      { name: "link", url: "https://placeholder" }
+    ]
+  },
+};
